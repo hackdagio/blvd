2da763e514fef1aa93d0f98775aa3d25ad9ff6cd
Ignacio Trujillo <ignacio@dawncoffee.com>
2014-10-25 14:16:36 -0300

    added json to urlencoded

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,7 +19,7 @@ controller('kaizenControllers', function ($scope, $http) {
 
 }).
 
-controller('loginController', function ($scope, $http) {
+controller('loginController', function ($scope, $http, transformRequestAsFormPost) {
 
   // hide error messages until submit event
   $scope.submitted = false;
@@ -30,7 +30,8 @@ controller('loginController', function ($scope, $http) {
 
     $http({
       method: 'post',
-      url: 'http://demo.kaizen.link/api/users/login'
+      url: 'http://demo.kaizen.link/api/users/login',
+      transformRequest: transformRequestAsFormPost,
     })
     .success(function (data, status, headers, config) {
       $scope.username = data.username;
@@ -52,4 +53,83 @@ controller('usuariosController', function ($scope) {
 controller('inicioController', function ($scope) {
 
 
-});
+})
+
+
+
+// I provide a request-transformation method that is used to prepare the outgoing
+// request as a FORM post instead of a JSON packet.
+.factory(
+  "transformRequestAsFormPost",
+  function() {
+
+        // I prepare the request data for the form post.
+        function transformRequest( data, getHeaders ) {
+
+          var headers = getHeaders();
+
+          headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
+
+          return( serializeData( data ) );
+
+        }
+
+
+        // Return the factory value.
+        return( transformRequest );
+
+
+        // ---
+        // PRVIATE METHODS.
+        // ---
+
+
+        // I serialize the given Object into a key-value pair string. This
+        // method expects an object and will default to the toString() method.
+        // --
+        // NOTE: This is an atered version of the jQuery.param() method which
+        // will serialize a data collection for Form posting.
+        // --
+        // https://github.com/jquery/jquery/blob/master/src/serialize.js#L45
+        function serializeData( data ) {
+
+            // If this is not an object, defer to native stringification.
+            if ( ! angular.isObject( data ) ) {
+
+              return( ( data == null ) ? "" : data.toString() );
+
+            }
+
+            var buffer = [];
+
+            // Serialize each key in the object.
+            for ( var name in data ) {
+
+              if ( ! data.hasOwnProperty( name ) ) {
+
+                continue;
+
+              }
+
+              var value = data[ name ];
+
+              buffer.push(
+                encodeURIComponent( name ) +
+                "=" +
+                encodeURIComponent( ( value == null ) ? "" : value )
+                );
+
+            }
+
+            // Serialize the buffer and clean it up for transportation.
+            var source = buffer
+            .join( "&" )
+            .replace( /%20/g, "+" )
+            ;
+
+            return( source );
+
+          }
+
+        }
+);
