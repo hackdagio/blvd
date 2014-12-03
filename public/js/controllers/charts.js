44ffebd9e833a2a56042ec3475a4098c14de9757
Ignacio Trujillo <ignacio@ignac.es>
2014-12-03 13:13:38 -0300

    Added date visualization with convert_date, tunning needed #12

--- public/js/controllers/charts.js
+++ public/js/controllers/charts.js
@@ -18,39 +18,23 @@ app.directive('metrics', function($http) {
       width: '='
     },
     link: function(scope, element) {
-      $( function(){
-
-        var responsive_width = '';
-
-        function WidthChange(mq) {
-          if (mq.matches) responsive_width = 100;
-          else responsive_width = scope.width;
-        };
-
-        if (matchMedia) {
-          var mq = window.matchMedia( "@media all and (max-width:767px)" );
-          mq.addListener(WidthChange);
-          WidthChange(mq);
-        };
-
-
-        var data = convert_dates(scope.data, 'Mes', '%Y-%m-%dT%H:%M:%S.%LZ');
+      
+      $(function(){
+        var data = convert_dates(scope.data, 'Mes', '%Y-%m-%d');
 
         data_graphic({
           data: data,
           height: scope.height,
-          width: responsive_width,
+          width: scope.width,
           target: element[0],
           x_accessor: 'Mes',
           y_accessor: 'Ponderado',
           animate_on_load: true,
           interpolate: 'basic',
           min_y: 70,
-          max_y: 71,
-          show_years: true
+          max_y: 71
         });
       });
-
     }
   };
 });
