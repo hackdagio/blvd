3272d26ac369012852367abf1f6313229e0de0cd
Ignacio Trujillo <ignacio@ignac.es>
2014-12-02 23:29:01 -0300

    Creating a directive to handle the metrics jQuery + Angular plugin #12

--- public/js/controllers/charts.js
+++ public/js/controllers/charts.js
@@ -1,33 +1,30 @@
 'use strict';
-app.controller('ponderadoController', ['$scope', 'indicadoresService', function ($scope, indicadoresService) {
+app.directive('metrics', ['$http', 'ngAuthSettings', function ($http, ngAuthSettings) {
 
-  indicadoresService.getIndicadores('history', 'general').then(function (results) {
-    $scope.data = results.data;
-  }, function (error) {
-    console.log('Error.');
-  });
-
-}]);
-
-app.directive('metrics', function($http) {
+  var serviceBase = ngAuthSettings.apiServiceBaseUri;
   return {
     restrict: 'E',
-    scope: {
-      data: '='
-    },
     link: function(scope, element) {
 
-      data_graphic({
-        title: "UFO Sightings",
-        description: "Yearly UFO sightings from 1945 to 2010.",
-        data: scope.data,
-        width: 400,
-        height: 250,
-        target: element[0],
-        x_accessor: "Mes",
-        y_accessor: "Ponderado",
-        interpolate: "monotone"
-      });
+      var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+      var success = function(result) {
+        data_graphic({
+          data: result,
+          width: 400,
+          height: 250,
+          target: element[0],
+          x_accessor: "Mes",
+          y_accessor: "Ponderado"
+        });
+      };
+
+      var error = function() {
+        console.log('Error.');
+      };
+
+      $http.get(serviceBase + 'api/kpis/history/general').success(success).error(error);
     }
   };
-});
+
+}]);
