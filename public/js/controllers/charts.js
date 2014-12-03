3eb68f4ac1125c9defd331ca637815bdef8e0910
Ignacio Trujillo <ignacio@ignac.es>
2014-12-03 12:55:25 -0300

    Converting date from API to bind data in Metrics #12

--- public/js/controllers/charts.js
+++ public/js/controllers/charts.js
@@ -18,16 +18,39 @@ app.directive('metrics', function($http) {
       width: '='
     },
     link: function(scope, element) {
+      $( function(){
 
-      data_graphic({
-        data: scope.data,
-        height: scope.height,
-        width: scope.width,
-        target: element[0],
-        x_accessor: 'Mes',
-        y_accessor: 'Ponderado',
-        animate_on_load: true
+        var responsive_width = '';
+
+        function WidthChange(mq) {
+          if (mq.matches) responsive_width = 100;
+          else responsive_width = scope.width;
+        };
+
+        if (matchMedia) {
+          var mq = window.matchMedia( "@media all and (max-width:767px)" );
+          mq.addListener(WidthChange);
+          WidthChange(mq);
+        };
+
+
+        var data = convert_dates(scope.data, 'Mes', '%Y-%m-%dT%H:%M:%S.%LZ');
+
+        data_graphic({
+          data: data,
+          height: scope.height,
+          width: responsive_width,
+          target: element[0],
+          x_accessor: 'Mes',
+          y_accessor: 'Ponderado',
+          animate_on_load: true,
+          interpolate: 'basic',
+          min_y: 70,
+          max_y: 71,
+          show_years: true
+        });
       });
+
     }
   };
 });
