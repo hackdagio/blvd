126450032fe2ec6364998e6cb1d7bede67337446
Ignacio Trujillo <ignacio@ignac.es>
2014-12-02 23:49:25 -0300

    Reverting to directive+controller strategy #12

--- public/js/controllers/charts.js
+++ public/js/controllers/charts.js
@@ -1,30 +1,29 @@
 'use strict';
-app.directive('metrics', ['$http', 'ngAuthSettings', function ($http, ngAuthSettings) {
+app.controller('ponderadoController', ['$scope', 'indicadoresService', function ($scope, indicadoresService) {
 
-  var serviceBase = ngAuthSettings.apiServiceBaseUri;
-  return {
-    restrict: 'E',
-    link: function(scope, element) {
-
-      var serviceBase = ngAuthSettings.apiServiceBaseUri;
-
-      var success = function(result) {
-        data_graphic({
-          data: result,
-          width: 400,
-          height: 250,
-          target: element[0],
-          x_accessor: "Mes",
-          y_accessor: "Ponderado"
-        });
-      };
-
-      var error = function() {
-        console.log('Error.');
-      };
-
-      $http.get(serviceBase + 'api/kpis/history/general').success(success).error(error);
-    }
-  };
+  indicadoresService.getIndicadores('history', 'general').then(function (results) {
+    $scope.data = results.data;
+  }, function (error) {
+    console.log('Error.');
+  });
 
 }]);
+
+app.directive('metrics', function($http) {
+  return {
+    restrict: 'E',
+    scope: {
+      data: '='
+    },
+    link: function(scope, element) {
+
+      data_graphic({
+        data: scope.data,
+        height: 300,
+        target: element[0],
+        x_accessor: "Mes",
+        y_accessor: "Ponderado"
+      });
+    }
+  };
+});
