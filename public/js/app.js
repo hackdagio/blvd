ef55008157e7230d71d4b18afcdc38a97d87b1e4
Ignacio Trujillo <ignacio@ignac.es>
2015-01-15 13:08:56 -0300

    :docs: nothing to see, move along

--- public/js/app.js
+++ public/js/app.js
@@ -104,3 +104,47 @@ var redirectIfAuthenticated = function(route) {
     return deferred.promise;
   }
 }
+
+
+app.factory('signalRHubProxy', ['$rootScope', 'signalRServer', 
+    function ($rootScope, signalRServer) {
+        function signalRHubProxyFactory(serverUrl, hubName, startOptions) {
+            var connection = $.hubConnection(signalRServer);
+            var proxy = connection.createHubProxy(hubName);
+            connection.start(startOptions).done(function () { });
+            
+            return {
+                on: function (eventName, callback) {
+                    proxy.on(eventName, function (result) {
+                        $rootScope.$apply(function () {
+                            if (callback) {
+                                callback(result);
+                            }
+                        });
+                    });
+                },
+                off: function (eventName, callback) {
+                    proxy.off(eventName, function (result) {
+                        $rootScope.$apply(function () {
+                            if (callback) {
+                                callback(result);
+                            }
+                        });
+                    });
+                },
+                invoke: function (methodName, callback) {
+                    proxy.invoke(methodName)
+                        .done(function (result) {
+                            $rootScope.$apply(function () {
+                                if (callback) {
+                                    callback(result);
+                                }
+                            });
+                        });
+                },
+                connection: connection
+            };
+        };
+
+        return signalRHubProxyFactory;    
+}]);
