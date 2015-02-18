d382be8acb33d09f85084a62457dfb0a5274a0eb
Ignacio Trujillo <ignacio@ignac.es>
2015-02-18 16:50:32 -0300

    :soon: playing with jade conditionals, and spinning up the hamster...

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var config = require('./../config.json');
 
 exports.index = function (req, res){
-  res.render('index', { title: config.product.name });
+  res.render('index', { 
+  	title: config.product.name,
+  	generator: config.app.name + ', powered by ' + config.api.name
+  });
 };
 
 exports.partials = function (req, res) {
