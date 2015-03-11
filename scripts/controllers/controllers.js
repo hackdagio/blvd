d25a5ce048704bae9757292b97ab310088ee87cc
Ignacio Trujillo <ignaces@ignac.es>
2015-03-11 16:38:25 -0300

    :zap: add new factory service that can read params from localstorage instead of the authentication object, epic

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -1,39 +1,112 @@
 'use strict';
 
-/*
-** Login Controller
-*/
-
-app.controller('loginController', ['$scope', '$location', 'authService', 
-  function ($scope, $location, authService) {
-
+app.controller('LoginCtrl', ['$scope', '$state', 'authService',
+  function ($scope, $state, authService) {
     $scope.loginData = {
-      userName: "",
-      password: ""
+      username: '',
+      password: ''
     };
 
-    $scope.message = "";
+    $scope.alert = {type: '', msg: ''};
 
     $scope.login = function () {
-      authService.login($scope.loginData).then(function (response) {
-        $location.path('/');
-      },
-      function (err) {
-        $scope.message = err.error_description;
-      });
+      authService.login($scope.loginData).
+        then(function (response) {
+          $state.go('index');
+        }, function (err) {
+          $scope.alert = {
+            type: 'danger',
+            msg: 'No puedes ingresar'
+          };
+        });
     };
   }
 ]);
 
-app.controller('navbarController', ['$scope', '$state', 'authService',
-  function ($scope, $state, authService) {
+
+app.controller('RequestCtrl', ['$scope', '$state', 'authService',
+  function($scope, $state, authService) {
+    $scope.requestData = {
+      idUser: ''
+    };
+
+    $scope.alert = {type: '',msg: ''};
+
+    $scope.request = function () {
+      authService.request($scope.requestData).
+        then(function (response) {
+          $scope.alert = {
+            type: 'success',
+            msg: 'Rut encontrado. Te hemos enviado el correo.'
+          };
+        }, function (err) {
+          $scope.alert = {
+            type: 'danger',
+            msg: 'Rut no encontrado'
+          };
+        });
+      };
+  }
+]);
+
+app.controller('SignupCtrl', ['$scope', '$timeout', '$state', '$stateParams', 'authService',
+  function($scope, $timeout, $state, $stateParams, authService) {
+
+    $scope.signupData = {
+      pwd: "",
+      pwdConfirmed: ""
+    };
+
+    $scope.alert = {type: '',msg: ''};
+
+    $scope.signup = function () {
+      authService.signup($scope.signupData, $stateParams.uid, $stateParams.token).
+        then(function (response) {
+          $scope.alert = {
+            type: 'success',
+            msg: 'Contraseña creada con éxito. En un instante, serás redirigido para que inicies sesión.'
+          };
+
+          return $timeout(function () {
+            $state.go('session.login', {}, { reload: true });
+          }, 5000);
+        }, function (err) {
+          $scope.alert = {
+            type: 'danger',
+            msg: 'Token no válido.'
+          };
+        });
+      };
+  }
+]);
+
+app.controller('AccountGeneralCtrl', ['$scope', 'localService',
+  function($scope, localService) {
+
+    var d = localService.getinfo('personaData');
+
+    $scope.PersonaInfo = {
+      id: d.id,
+      givenname: d.firstname + ' ' + d.middlename,
+      surname: d.surname + ' ' + d.lastname,
+      displayname: d.display_name,
+      email: d.email,
+      phone: d.phone
+    };
+
+  }
+]);
+
+app.controller('NavbarCtrl', ['$scope', '$state', 'localService', 'authService',
+  function ($scope, $state, localService, authService) {
+
+    $scope.personaData = localService.getinfo('personaData');
+    $scope.authentication = authService.authentication;
 
     $scope.logOut = function () {
       authService.logOut();
       $state.go('session.login');
     };
-
-    $scope.authentication = authService.authentication;
   }
 ]);
 
@@ -42,8 +115,8 @@ app.controller('bannerController', ['$scope',
     $scope.interval = 3000;
     $scope.slides = [
       { image: '//static.kaizen.link/entel/promos/promo_concurso.jpg' },
-  	 { image: '//static.kaizen.link/entel/promos/promo_san-valentin.jpg' },
-  	 { image: '//static.kaizen.link/entel/promos/promo_capacitacion.jpg' }
+     { image: '//static.kaizen.link/entel/promos/promo_san-valentin.jpg' },
+     { image: '//static.kaizen.link/entel/promos/promo_capacitacion.jpg' }
     ];
   }
 ]);
