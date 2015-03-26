3a1171ddc9550ca14dfb803953b729e1846ec8f1
Ignacio Trujillo <+@ignac.es>
2015-03-26 15:00:43 -0300

    :ghost::gem: update body-parser, express

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var config = require('../../config.json');
 
 exports.index = function (req, res){
-  res.render('index', { 
+  res.render('index', {
   	title: config.product.name,
   	generator: config.app.name + ', powered by ' + config.api.name,
   	api: config.api.url
