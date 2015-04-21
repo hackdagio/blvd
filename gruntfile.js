37343bccff3bcf03b79aae927b99e0d1f148847c
Ignacio Trujillo <+@ignac.es>
2015-04-21 17:26:59 -0300

    holy shit. if grunt were a girl, i'd **** her

--- gruntfile.js
+++ gruntfile.js
@@ -2,18 +2,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     coffee: {
-      glob_to_multiple: {
+      target: {
         expand: true,
-        flatten: true,
+        flatten: false,
         cwd: '../scripts/',
-        src: ['*.coffee'],
+        src: ['**/*.coffee'],
         dest: '../public/js/',
         ext: '.js'
+      }
+    },
+    uglify: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: '../public/js/',
+          src: ['**/*.js'],
+          dest: '../public/js/',
+          ext: '.min.js'
+        }]
       }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['coffee']);
+  grunt.registerTask('make-coffee', ['coffee']);
+  grunt.registerTask('make-uglify', ['uglify']);
 
 };
